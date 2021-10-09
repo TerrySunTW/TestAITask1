13ee00d8437880c3dcf87737db85200390a7a94b
Terry Sun <terry_sun@htc.com>
2021-10-09 23:49:38 +0800

    implement ParameterValidator and SendTaskToTaskQueue
    -Add ParameterValidator
    -Add SendTaskToTaskQueue
    -Add Example.py

--- task_assistant.py
+++ task_assistant.py
@@ -2,22 +2,56 @@
 import os
 import sys
 import argparse
+import pika
+import json
 
 
-def main(arguments):
-
+def ParameterValidator(arguments):
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('GithubLink', help="Github link for custom script and dockerfile, ex: https://github.com/TerrySunTW/TestAITask1.git")
-    parser.add_argument('DockerFilePath', help="dockerfile filename for runtime environment, system will using the docker environment to run your script")
-    parser.add_argument('TaskCommand', help="The commnad you need to run on the disribution system. (ex: python /scripts/test.py p1 p2 p3 )")
-    parser.add_argument('ArchiveFolderPath', help="Archive folderpath(default: ./out)")
-    parser.add_argument('ReportFilePath', help="Report filename(text file in json format)")
-
+    parser.add_argument('-TaskServer', required=True, help="TaskServer IP or FQDN. (ex: 10.116.116.108:5672)")
+    parser.add_argument('-Port', required=True, help="TaskServer port, (ex: 5672)")
+    parser.add_argument('-Github_TaskGit', required=True, help="Github Git for custom script and dockerfile. (ex: https://github.com/TerrySunTW/TestAITask1.git)")
+    parser.add_argument('-DockerFilePath', required=True, help="dockerfile filename for runtime environment, system will using the docker environment to run your script. (ex: docker/)")
+    parser.add_argument('-TaskCommand', required=True, help="The commnad you need to run on the disribution system. (ex: python cnn_handwrite.py p1 p2 p3 )")
+    parser.add_argument('-ArchiveFolderPath', required=True, help="Archive folderpath(ex:out)")
+    parser.add_argument('-ReportFilePath', required=True, help="Report filename(text file in json format). (ex:out/result.txt)")
+    parser.add_argument('-Github_TaskResult', required=True, help="Github Git for task archive files. (ex: git@github.com:terrysuntest/DataPool.git)")
+    parser.add_argument('-Github_TaskResult_BranchName', required=True, help="Branch Name in Github for task archive files. (ex:p1_p2_p3)")
     args = parser.parse_args(arguments)
-
     print(args)
+    return args
+
+def SendTaskToTaskQueue(args):
+    credentials = pika.PlainCredentials('guest', 'guest')  # mq用户名和密码
+    # 虚拟队列需要指定参数 virtual_host，如果是默认的可以不填。
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host = args.TaskServer,port = args.Port,virtual_host = '/',credentials = credentials))
+    channel=connection.channel()
+    # 声明消息队列，消息将在这个队列传递，如不存在，则创建
+    result = channel.queue_declare(queue = 'python-test',durable=True)
+
+    message=json.dumps({
+        'Github_TaskGit':args.Github_TaskGit,
+        'DockerFilePath':args.DockerFilePath,
+        'TaskCommand':args.TaskCommand,
+        'ArchiveFolderPath':args.ArchiveFolderPath,
+        'ReportFilePath':args.ReportFilePath,
+        'Github_TaskResult':args.Github_TaskResult,
+        'Github_TaskResult_BranchName':args.Github_TaskResult_BranchName
+        })
+    # 向队列插入数值 routing_key是队列名
+    channel.basic_publish(exchange = '',routing_key = 'python-test',body = message)
+    print(message)
+    connection.close()
+
+
+def main(arguments):
+    ValidatedArgs=ParameterValidator(arguments)
+    SendTaskToTaskQueue(ValidatedArgs)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
+
+
+
